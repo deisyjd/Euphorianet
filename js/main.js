d175b07ee561986ebba08bd39ec47e3e64aa6683
Julian Giovanni Castañeda Suarez <jcastaneda@euphoria-net.local>
2018-09-03 16:02:33 -0500

    [Jeanstyt] - Se agrego una nueva estructura en el resumen de la compra con la barra de progreso y se agrego el mensaje de acuerdo a lo sucedido.

--- js/main.js
+++ js/main.js
@@ -4,10 +4,13 @@
 $(function () {
     var total = $('span.tot').text().split("$");
     var progreso = (parseFloat(total[1]) / parseFloat(120.000)) * parseFloat(120.000);
-    if (parseInt(progreso) >= 100) {
-        progreso = 100;
+    //Validamos si ya supero los 120 y dependiendo de eso arrojamos un mensaje en pantalla
+    if (parseInt(progreso) <= 120) {
+        $('.texto-envio').text('El costo de envío deberá ser pagado a la transportadora.').css({ 'color': '#ff0080' });
+    } else {
+        $('.texto-envio').text('El costo de envío es gratis.').css({ 'color': '#1dd21d' });
     }
-    $("#progressbar").progressbar({
+    $("#progressbar, #progressbar_footer").progressbar({
         value: progreso
     });
 });
